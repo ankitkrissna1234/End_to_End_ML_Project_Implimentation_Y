8a5aae4b41c0a60868d51b7b192d217fbdf93cfa
ankitkrisshnna1234 <ankitkusahu7846@gmail.com>
2023-10-05 21:45:02 +0530

    model evaluation added

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from mlProject.pipeline.stage_03_data_transformation import (
     DataTransformationTrainingPipeline,
 )
 from mlProject.pipeline.stage_04_model_trainer import ModelTrainerTrainingPipeline
+from mlProject.pipeline.stage_05_model_evaluation import ModelEvaluationTrainingPipeline
 
 STAGE_NAME = "Data Ingestion stage"
 try:
@@ -43,6 +44,17 @@ STAGE_NAME = "Model Trainer stage"
 try:
     logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
     data_ingestion = ModelTrainerTrainingPipeline()
+    data_ingestion.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+STAGE_NAME = "Model Evaluation stage"
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    data_ingestion = ModelEvaluationTrainingPipeline()
     data_ingestion.main()
     logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
